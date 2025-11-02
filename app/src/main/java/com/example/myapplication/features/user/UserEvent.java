75c8f0eed9640e94a3c7989fcad27f6886c61795
humza <humzaaziz1705@gmail.com>
2025-11-02 15:17:40 -0700

    Updated UserEvent: added waitlist to UserEvent class

--- app/src/main/java/com/example/myapplication/features/user/UserEvent.java
+++ app/src/main/java/com/example/myapplication/features/user/UserEvent.java
@@ -2,6 +2,8 @@ package com.example.myapplication.features.user;
 
 import androidx.annotation.ColorInt;
 
+import java.util.List;
+
 class UserEvent {
     private final String id;
     private final String name;
@@ -11,9 +13,10 @@ class UserEvent {
     private final String descr;
     private final long endTimeMillis;
     private final int bannerColor;
+    private final List<String> waitlist;
 
     UserEvent(String id, String name, String location, String instructor,
-              String price, String descr, long endTimeMillis, @ColorInt int bannerColor) {
+              String price, String descr, long endTimeMillis, @ColorInt int bannerColor, List<String> waitlist) {
         this.id = id;
         this.name = name;
         this.location = location;
@@ -22,6 +25,7 @@ class UserEvent {
         this.descr = descr;
         this.endTimeMillis = endTimeMillis;
         this.bannerColor = bannerColor;
+        this.waitlist = waitlist;
     }
 
     String getId() { return id; }
@@ -30,6 +34,7 @@ class UserEvent {
     String getInstructor() { return instructor; }
     String getPrice() { return price; }
     String getDescr(){ return descr; }
+    List<String> getWaitlist() { return waitlist; }
     long getEndTimeMillis() { return endTimeMillis; }
     int getBannerColor() { return bannerColor; }
 }
