621741a58af0bb986d98e5f0670d7e26bdb13ec5
humza <humzaaziz1705@gmail.com>
2025-11-03 01:11:14 -0700

    Created Event Detail xml and added navigation to state

--- app/src/main/java/com/example/myapplication/features/user/UEventDetailFrag.java
+++ app/src/main/java/com/example/myapplication/features/user/UEventDetailFrag.java
@@ -1,6 +1,33 @@
 package com.example.myapplication.features.user;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import com.example.myapplication.R;
+import com.google.android.material.button.MaterialButton;
 
 public class UEventDetailFrag extends Fragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
+        return  inflater.inflate(R.layout.fragment_u_event_detail, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
+
+        super.onViewCreated(view, savedInstanceState);
+
+        MaterialButton backButton = view.findViewById(R.id.backButton);
+        backButton.setOnClickListener(x -> {
+            Navigation.findNavController(view).navigateUp();
+        });
+    }
 }
