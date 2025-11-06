83ff09615048268b3b5ccadcf8c9f5af6613546e
humza <humzaaziz1705@gmail.com>
2025-11-06 01:14:10 -0700

    Updated UEventDetailFrag.java: Image is retreived and displayed on detail view.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,8 @@ dependencies {
 
     //Cloudinary Android SDK
     implementation("com.cloudinary:cloudinary-android:3.0.2")
+
+    //Glide
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
 }
