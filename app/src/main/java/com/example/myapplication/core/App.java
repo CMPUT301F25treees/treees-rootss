1dc30e48aae360396e46fa6c51828f25798b97bb
humza <humzaaziz1705@gmail.com>
2025-11-06 00:42:58 -0700

    Updated App.java: initializing Cloudinary SDK at App entry

--- app/src/main/java/com/example/myapplication/core/App.java
+++ app/src/main/java/com/example/myapplication/core/App.java
@@ -1,10 +1,22 @@
 package com.example.myapplication.core;
 
 import android.app.Application;
+
+import com.cloudinary.android.MediaManager;
 import com.google.firebase.FirebaseApp;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class App extends Application {
     @Override
-    public void onCreate()
-    { super.onCreate(); FirebaseApp.initializeApp(this); }
+    public void onCreate() {
+        super.onCreate();
+        FirebaseApp.initializeApp(this);
+
+        Map<String, Object> config = new HashMap<>();
+        config.put("cloud_name", "dyb8t5n7k");
+        config.put("upload_preset", "treees_images");
+        MediaManager.init(this, config);
+    }
 }
