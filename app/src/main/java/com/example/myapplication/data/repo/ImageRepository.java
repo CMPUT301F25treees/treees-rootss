99d6550a9b53243e6ad7905ce39c47ea4dd433d6
humza <humzaaziz1705@gmail.com>
2025-11-06 00:54:34 -0700

    Updated ImageRepository.java: Upload method fro Cloudinary android sdk

--- app/src/main/java/com/example/myapplication/data/repo/ImageRepository.java
+++ app/src/main/java/com/example/myapplication/data/repo/ImageRepository.java
@@ -1,70 +1,57 @@
 package com.example.myapplication.data.repo;
 
+import android.content.Context;
+import android.net.Uri;
+import android.telecom.Call;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
+
+import com.cloudinary.android.MediaManager;
+import com.cloudinary.android.callback.ErrorInfo;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Map;
 
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.MediaType;
-import okhttp3.MultipartBody;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
 
 public class ImageRepository {
 
-    private static final String CLOUD_NAME = "dyb8t5n7k";
-    private static final String UPLOAD_PRESET = "treees_images";
-    private static final String ENDPOINT = "https://api.cloudinary.com/v1_1/dyb8t5n7k/image/upload";
-
-    private final OkHttpClient client = new OkHttpClient();
-
     public interface UploadCallback{
         void onSuccess(String secureUrl);
-        void onError(Exception e);
+        void onError(String e);
     }
 
-    public void uploadImage(@NonNull File imageFile, @NonNull UploadCallback callback){
-        RequestBody body = RequestBody.create(imageFile, MediaType.parse("image/*"));
+    public void uploadImage(Context context, Uri imageUri, UploadCallback callback){
+        MediaManager.get().upload(imageUri)
+                .callback(new com.cloudinary.android.callback.UploadCallback(){
 
-        MultipartBody request = new MultipartBody.Builder()
-                .setType(MultipartBody.FORM)
-                .addFormDataPart("file", imageFile.getName(), body)
-                .addFormDataPart("upload_preset", UPLOAD_PRESET)
-                .build();
+                    @Override
+                    public void onStart(String requestId) {
 
-        Request request1 = new Request.Builder()
-                .url(ENDPOINT)
-                .post(request)
-                .build();
+                    }
 
-        client.newCall(request1).enqueue(new Callback(){
+                    @Override
+                    public void onProgress(String requestId, long bytes, long totalBytes) {
 
-            @Override
-            public void onFailure(@NonNull Call call, @NonNull IOException e){
-                callback.onError(e);
-            }
+                    }
 
-            @Override
-            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException{
-                if(!response.isSuccessful()){
-                    callback.onError(new IOException("Upload failed: " + response.message()));
-                    return;
-                }
+                    @Override
+                    public void onSuccess(String requestId, Map resultData) {
+                        String secureUrl = resultData.get("secure_url").toString();
+                        callback.onSuccess((secureUrl));
+                    }
 
-                String responseBody = response.body().string();
-                JsonObject json = JsonParser.parseString(responseBody).getAsJsonObject();
-                String secureUrl = json.get("secure_url").getAsString();
-                callback.onSuccess(secureUrl);
-            }
-        });
+                    @Override
+                    public void onError(String requestId, ErrorInfo error) {
+                        callback.onError(error.getDescription());
+                    }
+
+                    @Override
+                    public void onReschedule(String requestId, ErrorInfo error) {
+
+                    }
+                });
     }
 }
