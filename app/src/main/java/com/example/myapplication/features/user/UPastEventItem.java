39d555680136233fcdf4ebbbe5e6e37a1580ffe3
Jaime Patel <jaimepatel123@gmail.com>
2025-11-26 22:14:35 -0700

    Javadocs and comment fix 4

--- app/src/main/java/com/example/myapplication/features/user/UPastEventItem.java
+++ app/src/main/java/com/example/myapplication/features/user/UPastEventItem.java
@@ -1,15 +1,40 @@
 package com.example.myapplication.features.user;
 
+/**
+ *  Fragment that allows users to see past event items.
+ */
 public class UPastEventItem {
 
+    /**
+     *  Event item fields.
+     */
     private String eventId;
+    /**
+     *  Title of the event.
+     */
     private String title;
+    /**
+     *  Price of the event.
+     */
     private String priceDisplay;
+    /**
+     *  Date of the event.
+     */
     private String date;
+    /**
+     *  Status of the event.
+     */
     private String status;
 
     public UPastEventItem() {}
 
+    /**
+     * @param eventId
+     * @param title
+     * @param priceDisplay
+     * @param date
+     * @param status
+     */
     public UPastEventItem(String eventId, String title,
                           String priceDisplay, String date, String status) {
         this.eventId = eventId;
@@ -19,9 +44,28 @@ public class UPastEventItem {
         this.status = status;
     }
 
+    /**
+     * @return
+     */
     public String getEventId()      { return eventId; }
+
+    /**
+     * @return
+     */
     public String getTitle()        { return title; }
+
+    /**
+     * @return
+     */
     public String getPriceDisplay() { return priceDisplay; }
+
+    /**
+     * @return
+     */
     public String getDate()         { return date; }
+
+    /**
+     * @return
+     */
     public String getStatus()       { return status; }
 }
