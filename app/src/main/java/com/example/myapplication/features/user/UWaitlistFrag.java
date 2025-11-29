af389425d315106dfcf3bd4809b91e7f142cf95c
humza <humzaaziz1705@gmail.com>
2025-11-28 23:59:35 -0700

    Added onCreateView

--- app/src/main/java/com/example/myapplication/features/user/UWaitlistFrag.java
+++ app/src/main/java/com/example/myapplication/features/user/UWaitlistFrag.java
@@ -6,9 +6,14 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.myapplication.R;
 import com.example.myapplication.data.firebase.FirebaseEventRepository;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.ArrayList;
 
 public class UWaitlistFrag extends Fragment implements UWaitlistAdapter.OnItemClickListener{
 
@@ -19,6 +24,19 @@ public class UWaitlistFrag extends Fragment implements UWaitlistAdapter.OnItemCl
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.fragment_u_waitlist, container, false);
 
+        recyclerView = view.findViewById(R.id.waitlistRecycler);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        adapter = new UWaitlistAdapter(new ArrayList<>(), this);
+        recyclerView.setAdapter(adapter);
+
+         repo = new FirebaseEventRepository();
+         curentUid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+         loadWaitlistEvents();
+
+         return view;
     }
+
 }
