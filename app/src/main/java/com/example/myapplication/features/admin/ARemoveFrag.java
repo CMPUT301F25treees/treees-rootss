f2589287bc3c0a316d752e4147bbfaca1a19f20e
Ishaan Ratanshi <i.ratanshi1@gmail.com>
2025-11-29 20:28:33 -0700

    default image for admin remove event poster

--- app/src/main/java/com/example/myapplication/features/admin/ARemoveFrag.java
+++ app/src/main/java/com/example/myapplication/features/admin/ARemoveFrag.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.Toast;
+import android.net.Uri;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
